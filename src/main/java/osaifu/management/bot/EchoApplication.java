ad8777f63b5248f3ffd8d1d80e76ee30077a5966
woodpeckeer <baske9141@gmail.com>
2020-12-19 14:57:09 +0900

    gradleファイル修正

--- src/main/java/osaifu/management/bot/EchoApplication.java
+++ src/main/java/osaifu/management/bot/EchoApplication.java
@@ -4,13 +4,12 @@
 package osaifu.management.bot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 
 @SpringBootApplication
 @LineMessageHandler
